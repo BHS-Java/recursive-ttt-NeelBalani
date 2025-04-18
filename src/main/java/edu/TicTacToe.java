9a7de663fde6a2d005b30d645fd145d46557465c
NeelBalani <2010725@apps.nsd.org>
2025-04-17 23:18:30 -0700

    TTT done

--- src/main/java/edu/TicTacToe.java
+++ src/main/java/edu/TicTacToe.java
@@ -32,21 +32,78 @@ public class TicTacToe {
 
     if(turn){
       board[x][y] = currentPlayer;
-      currentPlayer = 'O';
-      turn = !turn;
-      if(!winner){
-        playGame();
-      }
+      System.out.println(board[x][y]);
     }
     else{
       board[x][y] = currentPlayer;
-      currentPlayer = 'X';
+      System.out.println(board[x][y]);
+    }
+
+    for(int i = 0; i < board.length; i++){
+      if(checkRow(i, 0, currentPlayer) || checkCol(0,i,currentPlayer) || checkNDiag(i, currentPlayer) || checkPDiag(i, currentPlayer)){
+        winner = true;
+      }
+    }
+
+    if(!winner){
+      playGame();
       turn = !turn;
-      if(!winner){
-        playGame();
+      if(currentPlayer == 'O'){
+        currentPlayer = 'X';
+      }
+      else{
+        currentPlayer = 'O';
       }
     }
 
   };
 
+  public boolean checkRow(int row, int col, char player){
+    if(col == 2){
+      return board[row][col] == player;
+    }
+
+    if(board[row][col] != player){
+      return false;
+    }
+
+    return checkRow(row, col+1, player);
+  }
+
+  public boolean checkCol(int row, int col, char player){
+    if(row == 2){
+      return board[row][col] == player;
+    }
+
+    if(board[row][col] != player){
+      return false;
+    }
+
+    return checkCol(row+1, col, player);
+  }
+
+  public boolean checkNDiag(int n, char player){
+    if(n == 2){
+      return board[n][n] == player;
+    }
+
+    if(board[n][n] != player){
+      return false;
+    }
+
+    return checkNDiag(n+1, player);
+
+  }
+  public boolean checkPDiag(int n, char player){
+    if(n == 2){
+      return board[n][2-n] == player;
+    }
+
+    if(board[n][2-n] != player){
+      return false;
+    }
+
+    return checkNDiag(n+1, player);
+
+  }
 }
